8ed8521982a4f7a4834271ae5d2482b0944a24e1
LeifXu <ins@mask.io>
2024-05-14 23:07:38 +0800

    feat: add style of part

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,14 +12,22 @@ export const theme = extendBaseTheme({
     Tabs,
   },
   colors: {
+    black: '#000000',
     neutrals: {
+      1: '#FCFCFD',
       2: '#F4F5F6',
       5: '#B1B5C3',
+      6: '#353945',
+      8: '#141414',
+      9: '#05050A',
     },
     gradient: {
       purple:
         'linear-gradient(261deg, #D0D0FF 3.1%, #A996F7 33.87%, #7280FE 54.26%, #D3D6FE 104.35%)',
     },
+  },
+  shadows: {
+    card: '0px 5px 10px 0px rgba(0, 0, 0, 0.05), 0px 15px 30px 0px rgba(0, 0, 0, 0.05), 0px 20px 40px 0px rgba(0, 0, 0, 0.05)',
   },
   styles: {
     global: () => {
@@ -27,7 +35,7 @@ export const theme = extendBaseTheme({
         body: {
           fontFamily:
             'Inter, SFRounded, ui-rounded, "SF Pro Rounded", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-          backgroundColor: '#05050A',
+          backgroundColor: 'neutrals.9',
         },
       }
     },
