6d225f6d7a8d24f516ec262c7e234ef23c5e116a
LeifXu <ins@mask.io>
2024-05-16 12:20:49 +0800

    feat: add footer content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,28 +1,67 @@
 import type { FC } from 'react'
 import {
+  AspectRatio,
   Box,
   Center,
   Flex,
   Grid,
   Heading,
   Icon,
+  Image,
   VStack,
+  Link,
+  HStack,
 } from '@chakra-ui/react'
 import LogoSVG from '../../assets/logo-2.svg?react'
 import { t } from '@lingui/macro'
+import FooterImage from '../../assets/footer.webp'
+import { env } from '../../constants/env.ts'
+import DiscordSVG from '../../assets/social-media/discord.svg?react'
+import FacebookSVG from '../../assets/social-media/facebook.svg?react'
+import GithubSVG from '../../assets/social-media/github.svg?react'
+import TelegramSVG from '../../assets/social-media/telegram.svg?react'
+import RedditSVG from '../../assets/social-media/reddit.svg?react'
+import YoutubeSVG from '../../assets/social-media/youtube.svg?react'
+import MediumSVG from '../../assets/social-media/medium.svg?react'
+import TwitterSVG from '../../assets/social-media/twitter.svg?react'
+
+const SocialMedias = [
+  { icon: TwitterSVG, href: env.external.TWITTER_URL },
+  { icon: MediumSVG, href: env.external.MEDIUM_URL },
+  { icon: TelegramSVG, href: env.external.TELEGRAM_URL },
+  { icon: DiscordSVG, href: env.external.DISCORD_URL },
+  { icon: FacebookSVG, href: env.external.FACEBOOK_URL },
+  { icon: RedditSVG, href: env.external.REDDIT_URL },
+  { icon: GithubSVG, href: env.external.GITHUB_URL },
+  { icon: YoutubeSVG, href: env.external.YOUTUBE_URL },
+]
 
 export const Footer: FC = () => {
   return (
     <Flex direction="column" bg="neutrals.9" w="100%" mt="auto">
+      <AspectRatio ratio={891 / 61} w="100%">
+        <Image
+          src={FooterImage}
+          objectFit="cover"
+          draggable={false}
+          userSelect="none"
+        />
+      </AspectRatio>
       <Center w="100%" borderTop="1px solid" borderTopColor="neutrals.6">
         <Grid
-          templateColumns="repeat(4, 1fr)"
-          px={9}
-          py={12}
+          templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(4, 1fr)' }}
+          px={{ base: 3, md: 6, lg: 9 }}
+          py={{ base: 6, md: 12 }}
+          transition="200ms"
           w="100%"
           maxW="1440px"
+          gap={8}
         >
-          <VStack align="start" spacing={8}>
+          <VStack
+            align="start"
+            spacing={8}
+            gridColumn={{ base: '1/3', md: 'auto' }}
+          >
             <Icon as={LogoSVG} w="164px" h="auto" />
             <Heading
               color="neutrals.2"
@@ -31,19 +70,141 @@ export const Footer: FC = () => {
               fontWeight={400}
               fontSize="24px"
             >
-              Your Portal To The New, Open Internet.
+              {`Your Portal To The New, Open Internet.`}
             </Heading>
+          </VStack>
+          <VStack align="start" spacing={10}>
+            <Heading
+              as="h5"
+              fontSize="16px"
+              fontWeight={700}
+              lineHeight="24px"
+              color="neutrals.2"
+            >{`Integrations`}</Heading>
+            <VStack
+              as="ul"
+              listStyleType="none"
+              fontSize="14px"
+              fontWeight={700}
+              lineHeight="16px"
+              color="neutrals.4"
+              align="start"
+              w="100%"
+              spacing={6}
+              sx={{ li: { w: '100%' } }}
+            >
+              <li>
+                <Link
+                  target="_blank"
+                  href={env.external.GITHUB_URL}
+                  _hover={{ textDecoration: 'underline' }}
+                >
+                  {`Github`}
+                </Link>
+              </li>
+            </VStack>
+          </VStack>
+          <VStack align="start" spacing={10}>
+            <Heading
+              as="h5"
+              fontSize="16px"
+              fontWeight={700}
+              lineHeight="24px"
+              color="neutrals.2"
+            >{`Help`}</Heading>
+            <VStack
+              as="ul"
+              listStyleType="none"
+              fontSize="14px"
+              fontWeight={700}
+              lineHeight="16px"
+              color="neutrals.4"
+              align="start"
+              w="100%"
+              spacing={6}
+              sx={{ li: { w: '100%' } }}
+            >
+              <li>
+                <Link target="_blank" _hover={{ textDecoration: 'underline' }}>
+                  {`FAQs`}
+                </Link>
+              </li>
+              <li>
+                <Link target="_blank" _hover={{ textDecoration: 'underline' }}>
+                  {`Support`}
+                </Link>
+              </li>
+            </VStack>
+          </VStack>
+          <VStack
+            align="start"
+            spacing={10}
+            gridColumn={{ base: '1/3', md: 'auto' }}
+          >
+            <Heading
+              as="h5"
+              fontSize="16px"
+              fontWeight={700}
+              lineHeight="24px"
+              color="neutrals.2"
+            >{`Contact Us`}</Heading>
+            <Flex
+              wrap="wrap"
+              as="ul"
+              listStyleType="none"
+              fontSize="14px"
+              fontWeight={700}
+              lineHeight="16px"
+              w="100%"
+              gap={3}
+            >
+              {SocialMedias.map((media) => (
+                <li>
+                  <Center
+                    as="a"
+                    href={media.href}
+                    target="_blank"
+                    p={2}
+                    transition="100ms"
+                    _hover={{ transform: 'scale(1.1)' }}
+                    _active={{ transform: 'scale(0.9)' }}
+                  >
+                    <Icon as={media.icon} w={6} h={6} />
+                  </Center>
+                </li>
+              ))}
+            </Flex>
           </VStack>
         </Grid>
       </Center>
       <Center borderTop="1px solid" borderTopColor="neutrals.6" w="100%">
-        <Flex py={8} px={6} w="100%" maxW="1440px">
+        <Flex
+          py={{ base: 3, md: 6, lg: 8 }}
+          px={{ base: 3, md: 6 }}
+          transition="200ms"
+          w="100%"
+          maxW="1440px"
+          justify="space-between"
+          direction={{ base: 'column', sm: 'row' }}
+          gap={{ base: 3, sm: 0 }}
+        >
           <Box
             color="neutrals.4"
             fontSize="12px"
             fontWeight={400}
             lineHeight="150%"
           >{t`Since 2019 to Now ｜ Mask.io`}</Box>
+          <HStack
+            spacing={4}
+            color="neutrals.4"
+            fontSize="12px"
+            fontWeight={700}
+            lineHeight="20px"
+          >
+            <Link>Terms of Use</Link>
+            <Link>Privacy Policy</Link>
+            <Link>Cookie Policy</Link>
+          </HStack>
         </Flex>
       </Center>
     </Flex>
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -7,6 +7,14 @@ const InternalEnvSchema = z.object({})
 const ExternalEnvSchema = z.object({
   VERCEL_ENV: z.nativeEnum(VERCEL_NEV).default(VERCEL_NEV.Development),
   W3M_PROJECT_ID: z.string(),
+  TWITTER_URL: z.string().default('https://twitter.com/realMaskNetwork'),
+  TELEGRAM_URL: z.string().default('https://t.me/maskbook_group#telegram'),
+  DISCORD_URL: z.string().default('https://discord.gg/4SVXvj7'),
+  FACEBOOK_URL: z.string().default('https://www.facebook.com/masknetwork'),
+  REDDIT_URL: z.string().default('https://www.reddit.com/r/MaskNetwork'),
+  GITHUB_URL: z.string().default('https://github.com/DimensionDev'),
+  YOUTUBE_URL: z.string().default('https://www.youtube.com/c/MaskNetwork'),
+  MEDIUM_URL: z.string().default('https://www.youtube.com/c/MaskNetwork'),
 })
 
 export const env = {
@@ -19,5 +27,13 @@ export const env = {
   external: ExternalEnvSchema.parse({
     VERCEL_ENV: import.meta.env.VITE_VERCEL_ENV,
     W3M_PROJECT_ID: import.meta.env.VITE_W3M_PROJECT_ID,
+    TWITTER_URL: import.meta.env.VITE_TWITTER_URL,
+    TELEGRAM_URL: import.meta.env.VITE_TELEGRAM_URL,
+    DISCORD_URL: import.meta.env.VITE_DISCORD_URL,
+    FACEBOOK_URL: import.meta.env.VITE_FACEBOOK_URL,
+    REDDIT_URL: import.meta.env.VITE_REDDIT_URL,
+    GITHUB_URL: import.meta.env.VITE_GITHUB_URL,
+    YOUTUBE_URL: import.meta.env.VITE_YOUTUBE_URL,
+    MEDIUM_URL: import.meta.env.VITE_MEDIUM_URL,
   }),
 }
