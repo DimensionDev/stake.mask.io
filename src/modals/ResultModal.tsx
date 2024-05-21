3bf593f8a6eafb6054b258dc6470e44f63e3520b
Wukong Sun <swkatmask@gmail.com>
2024-05-21 17:23:09 +0800

    fix: mf-6272 adjust result modal style

--- src/modals/ResultModal.tsx
+++ src/modals/ResultModal.tsx
@@ -11,16 +11,22 @@ interface Props extends ModalProps {
 }
 export function ResultModal({ title, message, description, ...props }: Props) {
   return (
-    <BaseModal title={title} height={552} width={572} {...props}>
-      <VStack alignItems="center">
+    <BaseModal title={title} width={572} {...props}>
+      <VStack alignItems="center" spacing={6}>
         <Image src={new URL('../assets/astronaut.svg', import.meta.url).href} w={245} h={251} />
-        <Text color="neutrals.3" fontSize={32} fontWeight={700} lineHeight="40px" mt={8}>
+        <Text color="neutrals.3" fontSize={32} fontWeight={700} lineHeight="40px" textAlign="center">
           {message}
         </Text>
-        <Text color="neutrals.3" fontSize={16} mt={8}>
+        <Text color="neutrals.3" fontSize={16} fontWeight={700} textAlign="center">
           {description}
         </Text>
-        <Button className="purple-gradient-button" w="100%" rounded="40px" onClick={props.onClose}>{t`Done`}</Button>
+        <Button
+          className="purple-gradient-button"
+          w="100%"
+          mt="28px"
+          rounded="40px"
+          onClick={props.onClose}
+        >{t`Done`}</Button>
       </VStack>
     </BaseModal>
   )
