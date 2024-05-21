99b707f6c0d517756bee384fa6850c178514008b
Wukong Sun <swkatmask@gmail.com>
2024-05-21 12:23:47 +0800

    fix: pool info and calculation of share (#38)
    * fix: pool info
    
    * fix: calculation of share

--- src/hooks/usePoolInfo.ts
+++ src/hooks/usePoolInfo.ts
@@ -3,15 +3,17 @@ import urlcat from 'urlcat'
 import { FIREFLY_API_ROOT } from '../constants/api'
 import { fetchJSON } from '../helpers/fetchJSON'
 import { PoolInfoResponse } from '../types/api'
+import { usePoolStore } from '../store/poolStore'
 
 /**
  * Get info of current pool
  */
 export function usePoolInfo() {
+  const { poolId } = usePoolStore()
   return useQuery({
-    queryKey: ['pool-info'],
+    queryKey: ['pool-info', poolId],
     queryFn: async () => {
-      const url = urlcat(FIREFLY_API_ROOT, '/v1/mask_stake/pool_info', {})
+      const url = urlcat(FIREFLY_API_ROOT, '/v1/mask_stake/pool_info', { pool_id: poolId })
       const res = await fetchJSON<PoolInfoResponse>(url)
       return res.data
     },
