3d8ceffe24a2bff2b5612cec3784dd4140fa9313
Wukong Sun <swkatmask@gmail.com>
2024-06-11 18:32:25 +0800

    fix: poll interval (#122)

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -2,3 +2,5 @@ export const EMPTY_LIST = Object.freeze([]) as never[]
 export const EMPTY_OBJECT = Object.freeze({}) as Record<string, never>
 
 export const ZERO = BigInt(0)
+
+export const POLL_INTERVAL = 30 * 1000
